9c35703ce79d8b0730ebc7077f7a6015b5154244
吴伟泱的Mac <wwy@wuweiyangdeMac.local>
2017-06-24 11:58:28 +0800

    add eight files

---
+++ compress.js
@@ -0,0 +1,16 @@
+var fs = require('fs');
+var zlib = require('zlib');
+
+// 压缩input.txt文件为input.txt.gz
+// fs.createReadStream('input.txt')
+//   .pipe(zlib.createGzip())
+//   .pipe(fs.createWriteStream('input.txt.gz'));
+
+// console.log('文件压缩完成');
+
+// 解压input.txt.gz文件为input.txt
+fs.createReadStream('input.txt.gz')
+  .pipe(zlib.createGunzip())
+  .pipe(fs.createWriteStream('input.txt'));
+
+console.log('文件解压完成');
--- main.js
+++ main.js
@@ -1,26 +1,7 @@
-var fs = require('fs');
-var events = require('events');
+// var hello = require('./hello');
+// hello.world();
 
-// 阻塞式文件读取
-// var data = fs.readFileSync('input.txt');
-// console.log(data.toString());
-
-// 非阻塞式文件读取
-// fs.readFile('input.txt', function(err, data) {
-// 	if (err) return console.error(err);
-// 	console.log(data.toString());
-// });
-
-// 事件驱动
-var eventEmitter = new events.EventEmitter();
-var connectHandler = function connected() {
-	console.log('连接成功。');
-	eventEmitter.emit('data_received');
-}
-eventEmitter.on('connection', connectHandler);
-eventEmitter.on('data_received', function() {
-	console.log('数据接收成功。');
-});
-eventEmitter.emit('connection');
-
-console.log('程序执行结束！');
+var Hello = require('./hello');
+hello = new Hello();
+hello.setName('wwy');
+hello.sayHello();
